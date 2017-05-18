d6021893fb129a826db3b7c93f8a75629cbbcf07
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-05-18 16:00:34 +0300

    Last test v.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # get 'static_pages/home'
 
   # get 'static_pages/help'
-	root 'application#hello'
+  #	root 'application#hello'
   #
+  	root 'static_pages#home'
 	get  'static_pages/home'
 	get  'static_pages/help'
 	get  'static_pages/about'
