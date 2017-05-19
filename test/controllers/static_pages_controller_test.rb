251e9be8aadcbac0aa3788638c805a50d0587b94
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-05-19 16:15:45 +0300

    deleted cloning sample_app

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,6 +13,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
