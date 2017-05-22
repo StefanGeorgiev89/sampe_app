e77e3c6962f90c3b748064e106cd0158bfa6228a
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-05-22 17:29:28 +0300

    routes path

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
-	get 'foo/bar'
-	get 'foo/baz'
-  # get 'static_pages/home'
+	# get 'foo/bar'
+	# get 'foo/baz'
+ #  # get 'static_pages/home'
 
-  # get 'static_pages/help'
-  #	root 'application#hello'
-  # 
-   	root 'static_pages#home'
-	get  'static_pages/home'
-	get  'static_pages/help'
-	get  'static_pages/about'
-	get  'static_pages/contact'
-	get  'static_pages/book'
+ #  # get 'static_pages/help'
+ #  	root 'application#hello'
+ #  	root 'users#index'
+ #  # 
+ #   	root 'static_pages#home'
+	# get  'static_pages/home'
+	# get  'static_pages/help'
+	# get  'static_pages/about'
+	# get  'static_pages/contact'
+	# get  'static_pages/book'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
