c67cb9ecc7f54330a50bc59185d4cb2b7660cf81
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-05-23 13:24:05 +0300

    next step

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/help',    to: 'static_pages#help' #, as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
@@ -20,7 +20,5 @@ Rails.application.routes.draw do
 	# get  'static_pages/about'
 	# get  'static_pages/contact'
 	# get  'static_pages/book'
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
