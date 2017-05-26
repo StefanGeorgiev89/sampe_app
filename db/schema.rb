3c0cf9743b4f6e1a4dcf26725fa4294aed13ada3
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-05-26 16:45:41 +0300

    end user admin

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170526115441) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
